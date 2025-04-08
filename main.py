c62d7378ee7495129dde20ace51b6b741a9bb247
Mika Boumans <2303025boumans@zuyd.nl>
2025-04-08 12:06:00 +0200

    basis setup van git

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-class my_class(object):
-    pass
+import tkinter
+from tkinter import ttk
 
+import sv_ttk
 
+root = tkinter.Tk()
 
+button = ttk.Button(root, text="Click me!")
+button.pack()
 
+# This is where the magic happens
+sv_ttk.set_theme("dark")
+
+root.mainloop()
